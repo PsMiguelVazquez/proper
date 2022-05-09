ae18e846e961db78f7eefa67547633de1db9dae6
cesar <clopez@grupoola.net>
2022-05-09 10:40:12 -0500

    Add modules

--- sale_purchase_confirm/models/sale_order.py
+++ sale_purchase_confirm/models/sale_order.py
@@ -5,6 +5,5 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def action_confirm(self):
-        return super(SaleOrder).action_confirm()
+
 
