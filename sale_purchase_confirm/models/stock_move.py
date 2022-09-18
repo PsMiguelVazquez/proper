330f454b9a53ee19b213487681f4f90f08b05a18
clopez-ola <clopez@grupoola.net>
2022-09-18 17:10:47 -0500

    Add modules

--- sale_purchase_confirm/models/stock_move.py
+++ sale_purchase_confirm/models/stock_move.py
@@ -8,3 +8,10 @@ from odoo.exceptions import UserError
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
     origin = fields.Char(related='move_id.origin', string='Source', store=True)
+
+    def solict_reserved(self):
+        if self.origin:
+            sale = self.env['sale.order'].search([['name', '=', self.origin]])
+            user = self.env.user.ids
+            message = "El usuario "+str(user.name)+"\n"+"Requiere el producto "+str(self.product_id.name)
+            sale.message_post(body=message, partner_ids=sale.user_id.ids)
