8a49cffc03f8fcd2dd50c4d40f4fda3d2f2d7ff1
clopez-ola <clopez@grupoola.net>
2022-09-25 21:04:46 -0500

    Add modules

--- sale_purchase_confirm/models/stock_move.py
+++ sale_purchase_confirm/models/stock_move.py
@@ -16,7 +16,7 @@ class StockMoveLine(models.Model):
             message = "El usuario "+str(user.name)+"\n"+"Requiere el producto "+str(self.product_id.name)
             data = {
                 'res_id': sale.id,
-                'res_model_id': self.env['ir.model'].search([('model', '=', 'stock.move.line')]).id,
+                'res_model_id': self.env['ir.model'].search([('model', '=', 'sale.order')]).id,
                 'user_id': sale.user_id.id,
                 'summary': message,
                 'activity_type_id': self.env.ref('mail.mail_activity_data_meeting').id,
