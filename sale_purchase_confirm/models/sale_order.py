1afb96ee23eaf7fa2d9e73cadba87e6be3169a2c
clopez-ola <clopez@grupoola.net>
2022-11-30 12:40:51 -0600

    wsws

--- sale_purchase_confirm/models/sale_order.py
+++ sale_purchase_confirm/models/sale_order.py
@@ -315,7 +315,7 @@ class SaleInvoice(models.TransientModel):
             if not record.order_lines_ids:
                 record.sale_ids = self.env['sale.order'].browse(self.env.context.get('active_ids')).filtered(lambda x: x.state in ('sale', 'done'))
             else:
-                record.sale_ids = record.order_lines_ids
+                record.sale_ids = record.sale_ids
 
     @api.onchange('sale_ids')
     def set_orders(self):
