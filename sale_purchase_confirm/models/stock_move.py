9914da5069dca1cf6700ff32af8ebd4595b24d81
clopez-ola <clopez@grupoola.net>
2022-12-20 12:04:04 -0600

    wsws

--- sale_purchase_confirm/models/stock_move.py
+++ sale_purchase_confirm/models/stock_move.py
@@ -34,4 +34,4 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def _action_confirm(self, merge=True, merge_into=False):
-        return super(StockMove, self)._action_confirm(merge=False, merge_into)
+        return super(StockMove, self)._action_confirm(merge=False, merge_into=merge_into)
