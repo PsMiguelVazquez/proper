7b55e199c6302a0c3c06304c12a03db77bd2a7a3
clopez-ola <clopez@grupoola.net>
2023-01-12 21:45:12 -0600

    wsws

---
+++ vehiculos/models/sale.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+
+class Sale(models.Model):
+    _inherit = 'sale.order'
+    carrier_tracking_ref = fields.Char('No de Guia')
+
+    def write(self, values):
+        if 'carrier_tracking_ref' in values:
+            self.picking_ids.write({'carrier_tracking_ref': values['carrier_tracking_ref']})
+        return super(Sale, self).write(values)
