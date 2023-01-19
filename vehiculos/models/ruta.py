52aaa1abfb719c321b7c78a9742823c35d9d08a1
clopez-ola <clopez@grupoola.net>
2023-01-19 14:06:54 -0600

    wsws

--- vehiculos/models/ruta.py
+++ vehiculos/models/ruta.py
@@ -14,7 +14,7 @@ class CreacionRuta(Model):
     vehiculo=fields.Many2one('fleet.vehicle')
     ordenes=fields.Many2many('stock.picking')
     zona=fields.Selection([["SUR","SUR"],["NORTE","NORTE"],["PONIENTE","PONIENTE"],["ORIENTE","ORIENTE"],["CENTRO","CENTRO"],["DISTRIBUIDOR","DISTRIBUIDOR"],["MONTERREY","MONTERREY"],["CUERNAVACA","CUERNAVACA"],["GUADALAJARA","GUADALAJARA"],["QUERETARO","QUERETARO"],["CANCUN","CANCUN"],["VERACRUZ","VERACRUZ"],["PUEBLA","PUEBLA"],["TOLUCA","TOLUCA"],["LEON","LEON"],["COMODIN","COMODIN"],["VILLAHERMOSA","VILLAHERMOSA"],["MERIDA","MERIDA"],["VERACRUZ","VERACRUZ"],["ALTAMIRA","ALTAMIRA"]])
-    estado=fields.Selection([["borrador","Borrador"],["valido","Confirmado"]])
+    estado=fields.Selection([["borrador","Borrador"],["valido","Confirmado"]], default='borrador')
     odometro=fields.Integer()
     nivel_tanque=fields.Selection([["reserva","Reserva"],[".25","1/4"],[".5","1/2"],[".75","3/4"],["1","Lleno"]])
     tipo=fields.Selection([["local","Local"],["foraneo","Foraneo"],["guadalajara","Guadalajara"],["monterrey","Monterrey"],["queretaro","Querétaro"]])
