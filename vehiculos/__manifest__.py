fd08e6b18b01848c5398973c539beac770dfdd30
clopez-ola <clopez@grupoola.net>
2023-02-10 23:29:29 -0600

    dasd

--- vehiculos/__manifest__.py
+++ vehiculos/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'fleet', 'hr'],
+    'depends': ['base', 'stock', 'fleet', 'hr', 'sale'],
 
     # always loaded
     'data': [
