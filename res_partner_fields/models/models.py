dd4199437ec97a1418f07a21560f59904caced9e
Jonathan Alfaro <jonas4784@gmail.com>
2023-03-03 11:46:38 -0600

    UPDATE .  Campos Código de uso CFDI en contacto

--- res_partner_fields/models/models.py
+++ res_partner_fields/models/models.py
@@ -6,6 +6,15 @@ from odoo import models, fields, api
 class res_partner_fields(models.Model):
     _inherit = 'res.partner'
     sales_agent = fields.Many2one('res.users', string='Agente de ventas', store=True)
+    codigo_uso_cfdi = fields.Char(string="Código Uso CFDi", compute='_compute_codigo_uso_cfdi', store=False)
+
+
+    def _compute_codigo_uso_cfdi(self):
+        for record in self:
+            if record.x_studio_uso_de_cfdi:
+                record.codigo_uso_cfdi = str(record.x_studio_uso_de_cfdi)
+            else:
+                record.codigo_uso_cfdi = ''
 
     @api.onchange('sales_agent')
     def _on_change_sales_agent(self):
