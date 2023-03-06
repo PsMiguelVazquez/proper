bad44af27bd58899882ef591aa1aa16cd1de6cb9
Jonathan Alfaro <jonas4784@gmail.com>
2023-03-05 18:38:55 -0600

    UPDATE account_payment_proper, res_partner_fields, Puntos 5,23,33

--- res_partner_fields/models/models.py
+++ res_partner_fields/models/models.py
@@ -20,3 +20,12 @@ class res_partner_fields(models.Model):
     def _on_change_sales_agent(self):
         for record in self:
             self.x_nombre_agente_venta = record.sales_agent.name
+
+
+    @api.onchange('x_nivel_cliente')
+    def _compute_change_level(self):
+        for record in self:
+            partner = record.commercial_partner_id
+            if record.x_nivel_cliente:
+                message = "Se configuró el nivel de cliente " + record.x_nivel_cliente.x_name  +' para el usuario '+ record.name
+                partner[0].message_post(body=message, type="notification")
