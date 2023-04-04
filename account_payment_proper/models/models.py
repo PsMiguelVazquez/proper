ef4e938272f13e00d1a608c12b8e12619df2a61d
clopez-ola <clopez@grupoola.net>
2023-04-04 10:03:02 -0600

    dasd

--- account_payment_proper/models/models.py
+++ account_payment_proper/models/models.py
@@ -7,7 +7,7 @@ from odoo import models, fields, api
 class account_payment_proper(models.TransientModel):
     _inherit = 'account.payment.register'
     partner_bank_ref = fields.Many2one('res.partner.bank', string='Cuenta Bancaria Cliente')
-    registered_payment_id = fields.Integer(store=False)
+    registered_payment_id = fields.Many2many('account.payment')
 
     @api.onchange('partner_id')
     def deoman_banks(self):
@@ -19,21 +19,22 @@ class account_payment_proper(models.TransientModel):
 
     def _create_payments(self):
         res = super(account_payment_proper,self)._create_payments()
-        self.registered_payment_id = res.id
+        self.registered_payment_id = res.ids
         return res
 
     def action_create_payments(self):
         res = super(account_payment_proper,self).action_create_payments()
-        account_move_id = self.env['account.move'].search([('name','=',self.communication)])
-        pdf = self.env.ref('account.action_report_payment_receipt')._render(self.registered_payment_id)
-        b64_pdf = base64.b64encode(pdf[0])
-        attach_name = "Complemento de pago.pdf"
-        attachment = self.env['ir.attachment'].create({
-            'name': attach_name,
-            'type': 'binary',
-            'datas': b64_pdf,
-            'res_model': 'account.move',
-            'res_id': account_move_id.id,
-            'mimetype': 'application/pdf',
-        })
+        for payment in self.registered_payment_id:
+            #account_move_id = self.env['account.move'].search([('name','=',record.communication)])
+            pdf = self.env.ref('account.action_report_payment_receipt')._render(payment.id)
+            b64_pdf = base64.b64encode(pdf[0])
+            attach_name = "Complemento de pago.pdf"
+            attachment = self.env['ir.attachment'].create({
+                'name': attach_name,
+                'type': 'binary',
+                'datas': b64_pdf,
+                'res_model': 'account.move',
+                'res_id': payment.move_id.id,
+                'mimetype': 'application/pdf',
+            })
         return res
