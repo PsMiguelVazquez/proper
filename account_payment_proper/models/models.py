c756433082525916b997f9786fefeea162c3b30b
Jonathan Alfaro <jonas4784@gmail.com>
2023-04-04 10:52:48 -0600

    UPDATE account_payment_proper, Corrección al registrar pagos a mas de una factura a la vez

--- account_payment_proper/models/models.py
+++ account_payment_proper/models/models.py
@@ -19,21 +19,21 @@ class account_payment_proper(models.TransientModel):
 
     def _create_payments(self):
         res = super(account_payment_proper,self)._create_payments()
-        self.registered_payment_id = res.id
+        # self.registered_payment_id = res.id
         return res
 
     def action_create_payments(self):
         res = super(account_payment_proper,self).action_create_payments()
-        account_move_id = self.env['account.move'].search([('name','=',self.communication)])
-        pdf = self.env.ref('account.action_report_payment_receipt')._render(self.registered_payment_id)
-        b64_pdf = base64.b64encode(pdf[0])
-        attach_name = "Complemento de pago.pdf"
-        attachment = self.env['ir.attachment'].create({
-            'name': attach_name,
-            'type': 'binary',
-            'datas': b64_pdf,
-            'res_model': 'account.move',
-            'res_id': account_move_id.id,
-            'mimetype': 'application/pdf',
-        })
+        # account_move_id = self.env['account.move'].search([('name','=',self.communication)])
+        # pdf = self.env.ref('account.action_report_payment_receipt')._render(self.registered_payment_id)
+        # b64_pdf = base64.b64encode(pdf[0])
+        # attach_name = "Complemento de pago.pdf"
+        # attachment = self.env['ir.attachment'].create({
+        #     'name': attach_name,
+        #     'type': 'binary',
+        #     'datas': b64_pdf,
+        #     'res_model': 'account.move',
+        #     'res_id': account_move_id.id,
+        #     'mimetype': 'application/pdf',
+        # })
         return res
