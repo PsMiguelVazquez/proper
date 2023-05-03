055b5f172138d7f9eedba1a6abce34f95d45e221
Jonathan Alfaro <jonas4784@gmail.com>
2023-05-03 15:22:54 -0600

    UPDATE sale_purchase_confirm, helpdesk_partner_fields, Automatización de tareas en notas de crédito

---
+++ helpdesk_partner_fields/models/stock_picking.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def write(self, vals):
+        r = super(StockPicking, self).write(vals)
+        if r:
+            if self.env.context.get('default_pick_ids'):
+                picks = self.env['stock.picking'].search([('id', '=', self.env.context.get('default_pick_ids')[0])])
+                ticket = self.env['helpdesk.ticket'].search([('sale_order_id', '=', picks.sale_id.id)]).filtered(lambda x:  picks in x.picking_ids)
+                if ticket:
+                    if not self.env['mail.activity'].search(
+                            [('res_id', '=', ticket.id), ('activity_type_id', '=', 4), ('summary', '=', 'Ticket reasignado')]):
+                        ticket.activity_schedule(
+                            activity_type_id=4,
+                            summary="Ticket reasignado",
+                            note='Se ha completado la devolución y se le ha reasignado el ticket: ' + ticket.name,
+                            user_id= ticket.create_uid.id
+                        )
+        return r
