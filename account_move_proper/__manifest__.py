ddab5f6c52c89c205b5e45af6eff4cd7ba149832
Jonathan Alfaro <jonas4784@gmail.com>
2023-06-07 13:12:43 -0600

    Update Account_move_proper, Se agregaron los campos de fecha de entrega de la mercancia (obtenido desde el out) y fecha de recepción de la factura por parte de crédito

--- account_move_proper/__manifest__.py
+++ account_move_proper/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','product'],
+    'depends': ['base','account','product','l10n_mx_edi'],
 
     # always loaded
     'data': [
