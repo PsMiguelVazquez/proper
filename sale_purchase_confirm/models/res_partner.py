14c762839ec3e3ab0f50bd88fef24653900b9c8c
Jonathan Alfaro <jonas4784@gmail.com>
2023-06-06 18:03:19 -0600

    Update sale_purchase_confirm, Se valida que el RFC no esté duplicado en el cliente

--- sale_purchase_confirm/models/res_partner.py
+++ sale_purchase_confirm/models/res_partner.py
@@ -1,4 +1,5 @@
 from odoo import api,models,fields,_
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -8,3 +9,11 @@ class ResPartner(models.Model):
         for record in self:
             operation = record.credit_limit-record.credit
             record.credit_rest =0 if operation<0 else operation
+
+    # _sql_constraints = [('niv','unique(niv)','Ya existe otro cliente con ese RFC')]
+    @api.constrains('vat')
+    def _check_rfc(self):
+        for record in self:
+            cliente = self.env['res.partner'].search([('vat','ilike',record.vat),('id','!=',record.id)])
+            if cliente:
+                raise ValidationError('Ya existe otro cliente ('+ ', '.join(cliente.mapped('name'))+') con ese RFC')
