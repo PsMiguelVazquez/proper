54eb01627fa3cc1846dee3f615a40dd7a2aac3fd
jonathanalfaro <jonas4784@gmail.com>
2023-06-18 18:17:21 -0600

    Update kardex_producto, Segunda parte kardex

--- kardex_productos/models/stock_picking.py
+++ kardex_productos/models/stock_picking.py
@@ -27,8 +27,7 @@ class StockMoveLine(models.Model):
                 # Si es IN busca el último movimiento que tenga el mismo destino "A" en los movimientos anteriores
                 if record.picking_type_id and record.picking_type_id.id == 101:
                     movs_mismo_dia = self.filtered(lambda x: x.date == record.date)
-                    movs_ant = self.filtered(lambda x: x.date < record.date
-                                                       and x.location_dest_id == record.location_dest_id)
+                    movs_ant = self.filtered(lambda x: x.date < record.date and x.location_dest_id == record.location_dest_id or x.location_dest_id == record.location_id)
                     # Si se hicieron entradas parciales el mismo dia y misma fecha
                     if len(movs_mismo_dia) > 1:
                         # Si es el primer movimiento, el movimiento anterior es el movimiento inmediato anterior
@@ -47,14 +46,30 @@ class StockMoveLine(models.Model):
                     record.saldo = saldo_mov_anterior + entradas - salidas
                 else:
                     # Si es un pick obtiene el saldo anterior donde el origen es igual al destino en el movimiento anterior
-                    if record.picking_type_id and record.picking_type_id == 103:
+                    if record.location_dest_id.id == 5:
+                        movs_mismo_dia = self.filtered(lambda x: x.date == record.date)
+                        movs_ant = self.filtered(lambda
+                                                     x: x.date < record.date and x.location_dest_id.id == 187 or x.location_id.id == record.location_id.id)
+                        # Si se hicieron entradas parciales el mismo dia y misma fecha
+                        #if len(movs_mismo_dia) > 1:
+                            # Si es el primer movimiento, el movimiento anterior es el movimiento inmediato anterior
+                            # a la fecha del movimiento actual
+                        #    if record == movs_mismo_dia[0]:
+                        #        mov_ant = movs_ant.filtered(lambda x: x.id != record.id and x.date != record.date)[-1]
+                        #    else:
+                        #        mov_ant = movs_mismo_dia[0]
+                        if movs_ant:
+                            mov_ant = movs_ant[-1]
+                            saldo_mov_anterior = mov_ant.saldo
+                        else:
+                            saldo_mov_anterior = 0
+                        salidas = record.qty_done
+                        entradas = 0.0
+                        record.entradas = entradas
+                        record.salidas = salidas
+                        record.saldo = saldo_mov_anterior + entradas - salidas
+                    else:
                         record.entradas = 0
                         record.salidas = 0
                         record.saldo = 0
-                    else:
-                        record.picking_type_id.id
-                        record.location_id
-                        record.entradas = 0.0
-                        record.salidas = 0.0
-                        record.saldo = entradas - salidas
 
