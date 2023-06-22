4839f5a2d0ccc8fba2b4f0e074450a61b51f989a
jonathanalfaro <jonas4784@gmail.com>
2023-06-22 01:49:08 -0600

    Update Kardex_productos, Implementación del kardex de productos

--- kardex_productos/models/stock_picking.py
+++ kardex_productos/models/stock_picking.py
@@ -6,61 +6,116 @@ from odoo.exceptions import UserError
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
-    entradas = fields.Float('Entradas', compute='_compute_kardex')
-    salidas = fields.Float('Salidas', compute='_compute_kardex')
-    saldo = fields.Float('Saldo', compute='_compute_kardex')
-    localidad = fields.Char(string='Localidad',related='location_id.location_id.name',store=True)
+    entradas_origen = fields.Float('Entradas origen', compute='_compute_kardex')
+    salidas_origen = fields.Float('Salidas origen', compute='_compute_kardex')
+    saldo_origen = fields.Float('Saldo origen', compute='_compute_kardex')
+    entradas_destino = fields.Float('Entradas destino', compute='_compute_kardex')
+    salidas_destino = fields.Float('Salidas destino', compute='_compute_kardex')
+    saldo_destino = fields.Float('Saldo destino', compute='_compute_kardex')
+    localidad_origen = fields.Char(string='Localidad de Origen',related='location_id.location_id.name')
+    localidad_destino = fields.Char(string='Localidad de Destino',related='location_dest_id.location_id.name')
+
 
     def _compute_kardex(self):
         for record in self:
-            entradas = 0.0
-            salidas = 0.0
-            saldo = 0.0
+            record.entradas_origen = 0.0
+            record.salidas_origen = 0.0
+            record.saldo_origen = 0.0
+            record.entradas_destino = 0.0
+            record.salidas_destino = 0.0
+            record.saldo_destino = 0.0
             # Si es un ajuste de inventario el saldo es la cantidad hecha en ese movimiento
             if record.location_id.id == 14:
-                localidad_padre = record.location_dest_id.location_id
-                record.entradas = record.qty_done
-                record.salidas = 0.0
-                record.saldo = record.entradas - record.salidas
-            else:
-                localidad_padre = record.location_dest_id.location_id
-                movs_ant = self.env['stock.move.line'].search(
-                    [('date', '<=', record.date), ('id', '<', record.id), ('product_id', '=', record.product_id.id),
-                     ('state', '=', 'done')], order='id asc')
-                if record.picking_type_id.code == 'outgoing':
-                    movs_ant = movs_ant.filtered(
-                                  lambda y: (y.location_dest_id.location_id == record.location_id.location_id and y.location_id.id == 14)  # Ajustes de almacén
-                                  or (y.location_id.location_id == record.location_id.location_id and y.picking_type_id.code == 'outgoing')  # Salidas
-                                  or (y.location_dest_id.location_id == record.location_id.location_id and y.picking_type_id.code == 'incoming')  # Entradas
-                                  or (y.picking_type_id.code == 'internal' and y.picking_type_id.id == 202 and y.location_dest_id.location_id == record.location_id.location_id))  # Internas
-                    salidas = record.qty_done
-                    entradas = 0.0
-                elif record.picking_type_id.code == 'incoming':
-                    movs_ant = movs_ant.filtered(
-                                  lambda y: (y.location_dest_id == record.location_dest_id and y.location_id.id == 14)  # Ajustes de almacén
-                                  or (y.location_id.location_id == record.location_dest_id.location_id and y.picking_type_id.code == 'outgoing')  # Salidas
-                                  or (y.location_dest_id == record.location_dest_id and y.picking_type_id.code == 'incoming')  # Entradas
-                                  or (y.picking_type_id.code == 'internal' and y.picking_type_id.id == 202 and y.location_dest_id == record.location_dest_id))  # Internas
-                    entradas = record.qty_done
-                    salidas = 0.0
-                elif record.picking_type_id.code == 'internal' and record.picking_type_id.id == 202:
-                    movs_ant = movs_ant.filtered(
-                                  lambda y: (y.location_dest_id == record.location_dest_id and y.location_id.id == 14)  # Ajustes de almacén
-                                  or (y.location_id.location_id == record.location_dest_id.location_id and y.picking_type_id.code == 'outgoing')  # Salidas
-                                  or (y.location_dest_id == record.location_dest_id and y.picking_type_id.code == 'incoming')  # Entradas
-                                  or (y.picking_type_id.code == 'internal' and y.picking_type_id.id == 202 and y.location_dest_id == record.location_dest_id))  # Internas
-                    entradas = record.qty_done
-                    salidas = 0.0
-                else:
-                    record.entradas = entradas
-                    record.salidas = salidas
-                    record.saldo = entradas - salidas
-                    return
-                if movs_ant:
-                    mov_ant = movs_ant[-1]
-                    saldo_mov_anterior = mov_ant.saldo
-                else:
-                    saldo_mov_anterior = 0
-                record.entradas = entradas
-                record.salidas = salidas
-                record.saldo = saldo_mov_anterior + entradas - salidas
+                record.entradas_destino = record.qty_done
+                record.saldo_destino = record.entradas_destino
+            elif '/IN/' in record.reference or '/INT/' in record.reference or '/OUT/' in record.reference or 'INT' in record.reference:
+                if '/IN/' in record.reference:
+                    record.entradas_destino = record.qty_done
+                    movs_origen = self.env['stock.move.line'].search(
+                        [('date', '<=', record.date), ('product_id', '=', record.product_id.id),
+                         ('state', '=', 'done')], order='date asc').filtered \
+                        (lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.reference)  # Salidas
+                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.reference)  # entradas
+                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.reference or 'INT' in y.reference))  # Internas
+                                   or (y.localidad_destino == record.localidad_destino and y.location_id.id == 14)  # Ajustes
+                                   and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                    if movs_origen:
+                        movs_mismo_dia = movs_origen.filtered(lambda z: z.date == record.date and z.id < record.id)
+                        if movs_mismo_dia:
+                            ultimo_mov_origen = movs_mismo_dia[-1]
+                        else:
+                            if len(movs_origen) > 1:
+                                ultimo_mov_origen = movs_origen[-2]
+                            else:
+                                ultimo_mov_origen = movs_origen[-1]
+                        if record.localidad_destino == ultimo_mov_origen.localidad_origen:
+                            saldo_destino = ultimo_mov_origen.saldo_origen
+                            record.saldo_destino = saldo_destino + record.qty_done
+                        else:
+                            saldo_destino = ultimo_mov_origen.saldo_destino
+                            record.saldo_destino = saldo_destino + record.qty_done
+                    else:
+                        record.saldo_destino = record.qty_done
+                elif '/OUT/' in record.reference:
+                    record.salidas_origen = record.qty_done
+                    movs_origen = self.env['stock.move.line'].search([('date', '<=', record.date), ('product_id', '=', record.product_id.id),
+                     ('state', '=', 'done')], order='date asc').filtered\
+                                    (lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
+                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
+                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
+                                    or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
+                                    and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                    if movs_origen:
+                        movs_mismo_dia = movs_origen.filtered(lambda z: z.date == record.date and z.id < record.id)
+                        if movs_mismo_dia:
+                            ultimo_mov_origen = movs_mismo_dia[-1]
+                        else:
+                            if len(movs_origen) > 1:
+                                ultimo_mov_origen = movs_origen[-2]
+                            else:
+                                ultimo_mov_origen = movs_origen[-1]
+                        if record.localidad_origen == ultimo_mov_origen.localidad_origen:
+                            saldo_origen = ultimo_mov_origen.saldo_origen
+                            record.saldo_origen = saldo_origen - record.qty_done
+                        else:
+                            saldo_destino = ultimo_mov_origen.saldo_destino
+                            record.saldo_origen = saldo_destino - record.qty_done
+                    else:
+                        record.saldo_origen
+                elif 'INT' in record.reference:
+                    movs_ant = self.env['stock.move.line'].search(
+                        [('id', '<', record.id), ('product_id', '=', record.product_id.id),
+                         ('state', '=', 'done')], order='id asc')
+                    movs_origen = self.env['stock.move.line'].search([('date', '<=', record.date), ('id', '<', record.id), ('product_id', '=', record.product_id.id),
+                                    ('state', '=', 'done')], order='date asc').filtered(lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
+                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
+                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
+                                    or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
+                                    and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                    movs_destino = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.reference)  # Salidas
+                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.reference)  # entradas
+                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.reference or 'INT' in y.reference))  # Internas
+                                   or (y.localidad_destino == record.localidad_destino and y.location_id.id == 14)  # Ajustes
+                                   and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                    if movs_origen:
+                        if movs_origen[-1].localidad_origen == record.localidad_origen:
+                            saldo_movs_origen = movs_origen[-1].saldo_origen
+                        else:
+                            saldo_movs_origen = movs_origen[-1].saldo_destino
+                    else:
+                        saldo_movs_origen = 0
+                    if movs_destino:
+                        if movs_destino[-1].localidad_origen == record.localidad_destino:
+                            saldo_movs_destino = movs_destino[-1].saldo_origen
+                        else:
+                            saldo_movs_destino = movs_destino[-1].saldo_destino
+                    else:
+                        saldo_movs_destino = 0.0
+                    record.salidas_origen = record.qty_done
+                    record.entradas_destino = record.qty_done
+                    record.saldo_origen = saldo_movs_origen - record.salidas_origen
+                    record.saldo_destino = saldo_movs_destino + record.entradas_destino
+
+
+
+
