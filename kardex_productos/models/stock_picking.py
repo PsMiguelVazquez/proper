65bc280fd0ab150417b848fbe18274fd05554a08
jonathanalfaro <jonas4784@gmail.com>
2023-06-22 12:07:01 -0600

    Update kardex_producto, Se corrigieron errores al generar el kardex

--- kardex_productos/models/stock_picking.py
+++ kardex_productos/models/stock_picking.py
@@ -14,11 +14,14 @@ class StockMoveLine(models.Model):
     saldo_destino = fields.Float('Saldo destino', compute='_compute_kardex')
     localidad_origen = fields.Char(string='Localidad de Origen',related='location_id.location_id.name')
     localidad_destino = fields.Char(string='Localidad de Destino',related='location_dest_id.location_id.name')
+    tipo_mov = fields.Char(string='Tipo de movimiento', compute='_compute_kardex')
 
 
     def _compute_kardex(self):
         movs_ant = self.env['stock.move.line']
-        for record in self:
+        movs_producto = self.env['stock.move.line'].search([('product_id','=',self[0].product_id.id),('state','=','done')], order='date asc')
+        for record in movs_producto:
+            record.tipo_mov = record.reference if record.reference else '/' +record.picking_type_id.sequence_code +'/'
             record.entradas_origen = 0.0
             record.salidas_origen = 0.0
             record.saldo_origen = 0.0
@@ -30,15 +33,15 @@ class StockMoveLine(models.Model):
                 record.entradas_destino = record.qty_done
                 record.saldo_destino = record.entradas_destino
                 movs_ant|=record
-            elif '/IN/' in record.reference or '/INT/' in record.reference or '/OUT/' in record.reference or 'INT' in record.reference:
-                if '/IN/' in record.reference:
+            elif '/IN/' in record.tipo_mov or '/INT/' in record.tipo_mov or '/OUT/' in record.tipo_mov or 'INT' in record.tipo_mov:
+                if '/IN/' in record.tipo_mov:
                     record.entradas_destino = record.qty_done
                     movs_origen = movs_ant.filtered \
-                        (lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.reference)  # Salidas
-                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.reference)  # entradas
-                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.reference or 'INT' in y.reference))  # Internas
+                        (lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.tipo_mov)  # Salidas
+                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.tipo_mov)  # entradas
+                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.tipo_mov or 'INT' in y.tipo_mov))  # Internas
                                    or (y.localidad_destino == record.localidad_destino and y.location_id.id == 14)  # Ajustes
-                                   and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                                   and '/PACK/' not in y.tipo_mov and '/PICK/' not in y.tipo_mov)
                     if movs_origen:
                         ultimo_mov_origen = movs_origen[-1]
                         if record.localidad_destino == ultimo_mov_origen.localidad_origen:
@@ -49,14 +52,14 @@ class StockMoveLine(models.Model):
                             record.saldo_destino = saldo_destino + record.qty_done
                     else:
                         record.saldo_destino = record.qty_done
-                elif '/OUT/' in record.reference:
+                elif '/OUT/' in record.tipo_mov:
                     record.salidas_origen = record.qty_done
                     movs_origen = movs_ant.filtered\
-                                    (lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
-                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
-                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
+                                    (lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.tipo_mov) #Salidas
+                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.tipo_mov) # entradas
+                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.tipo_mov or 'INT' in y.tipo_mov)) # Internas
                                     or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
-                                    and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                                    and '/PACK/' not in y.tipo_mov and '/PICK/' not in y.tipo_mov)
                     if movs_origen:
                         ultimo_mov_origen = movs_origen[-1]
                         if record.localidad_origen == ultimo_mov_origen.localidad_origen:
@@ -67,17 +70,17 @@ class StockMoveLine(models.Model):
                             record.saldo_origen = saldo_destino - record.qty_done
                     else:
                         record.saldo_origen
-                elif 'INT' in record.reference:
-                    movs_origen = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
-                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
-                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
+                elif 'INT' in record.tipo_mov:
+                    movs_origen = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.tipo_mov) #Salidas
+                                    or (y.localidad_destino == record.localidad_origen and '/IN/' in y.tipo_mov) # entradas
+                                    or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.tipo_mov or 'INT' in y.tipo_mov)) # Internas
                                     or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
-                                    and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
-                    movs_destino = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.reference)  # Salidas
-                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.reference)  # entradas
-                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.reference or 'INT' in y.reference))  # Internas
+                                    and '/PACK/' not in y.tipo_mov and '/PICK/' not in y.tipo_mov)
+                    movs_destino = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.tipo_mov)  # Salidas
+                                   or (y.localidad_destino == record.localidad_destino and '/IN/' in y.tipo_mov)  # entradas
+                                   or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.tipo_mov or 'INT' in y.tipo_mov))  # Internas
                                    or (y.localidad_destino == record.localidad_destino and y.location_id.id == 14)  # Ajustes
-                                   and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
+                                   and '/PACK/' not in y.tipo_mov and '/PICK/' not in y.tipo_mov)
                     if movs_origen:
                         if movs_origen[-1].localidad_origen == record.localidad_origen:
                             saldo_movs_origen = movs_origen[-1].saldo_origen
