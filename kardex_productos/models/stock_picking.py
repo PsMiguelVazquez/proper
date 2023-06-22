634199d1b96999e661cbc59199014cf7e4aa5c34
jonathanalfaro <jonas4784@gmail.com>
2023-06-22 07:05:07 -0600

    Update Kardex_productos, Implementación del kardex de productos

--- kardex_productos/models/stock_picking.py
+++ kardex_productos/models/stock_picking.py
@@ -17,6 +17,7 @@ class StockMoveLine(models.Model):
 
 
     def _compute_kardex(self):
+        movs_ant = self.env['stock.move.line']
         for record in self:
             record.entradas_origen = 0.0
             record.salidas_origen = 0.0
@@ -28,26 +29,18 @@ class StockMoveLine(models.Model):
             if record.location_id.id == 14:
                 record.entradas_destino = record.qty_done
                 record.saldo_destino = record.entradas_destino
+                movs_ant|=record
             elif '/IN/' in record.reference or '/INT/' in record.reference or '/OUT/' in record.reference or 'INT' in record.reference:
                 if '/IN/' in record.reference:
                     record.entradas_destino = record.qty_done
-                    movs_origen = self.env['stock.move.line'].search(
-                        [('date', '<=', record.date), ('product_id', '=', record.product_id.id),
-                         ('state', '=', 'done')], order='date asc').filtered \
+                    movs_origen = movs_ant.filtered \
                         (lambda y: (y.localidad_origen == record.localidad_destino and '/OUT/' in y.reference)  # Salidas
                                    or (y.localidad_destino == record.localidad_destino and '/IN/' in y.reference)  # entradas
                                    or ((y.localidad_origen == record.localidad_destino or y.localidad_destino == record.localidad_destino) and ('/INT/' in y.reference or 'INT' in y.reference))  # Internas
                                    or (y.localidad_destino == record.localidad_destino and y.location_id.id == 14)  # Ajustes
                                    and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
                     if movs_origen:
-                        movs_mismo_dia = movs_origen.filtered(lambda z: z.date == record.date and z.id < record.id)
-                        if movs_mismo_dia:
-                            ultimo_mov_origen = movs_mismo_dia[-1]
-                        else:
-                            if len(movs_origen) > 1:
-                                ultimo_mov_origen = movs_origen[-2]
-                            else:
-                                ultimo_mov_origen = movs_origen[-1]
+                        ultimo_mov_origen = movs_origen[-1]
                         if record.localidad_destino == ultimo_mov_origen.localidad_origen:
                             saldo_destino = ultimo_mov_origen.saldo_origen
                             record.saldo_destino = saldo_destino + record.qty_done
@@ -58,22 +51,14 @@ class StockMoveLine(models.Model):
                         record.saldo_destino = record.qty_done
                 elif '/OUT/' in record.reference:
                     record.salidas_origen = record.qty_done
-                    movs_origen = self.env['stock.move.line'].search([('date', '<=', record.date), ('product_id', '=', record.product_id.id),
-                     ('state', '=', 'done')], order='date asc').filtered\
+                    movs_origen = movs_ant.filtered\
                                     (lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
                                     or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
                                     or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
                                     or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
                                     and '/PACK/' not in y.reference and '/PICK/' not in y.reference)
                     if movs_origen:
-                        movs_mismo_dia = movs_origen.filtered(lambda z: z.date == record.date and z.id < record.id)
-                        if movs_mismo_dia:
-                            ultimo_mov_origen = movs_mismo_dia[-1]
-                        else:
-                            if len(movs_origen) > 1:
-                                ultimo_mov_origen = movs_origen[-2]
-                            else:
-                                ultimo_mov_origen = movs_origen[-1]
+                        ultimo_mov_origen = movs_origen[-1]
                         if record.localidad_origen == ultimo_mov_origen.localidad_origen:
                             saldo_origen = ultimo_mov_origen.saldo_origen
                             record.saldo_origen = saldo_origen - record.qty_done
@@ -83,11 +68,7 @@ class StockMoveLine(models.Model):
                     else:
                         record.saldo_origen
                 elif 'INT' in record.reference:
-                    movs_ant = self.env['stock.move.line'].search(
-                        [('id', '<', record.id), ('product_id', '=', record.product_id.id),
-                         ('state', '=', 'done')], order='id asc')
-                    movs_origen = self.env['stock.move.line'].search([('date', '<=', record.date), ('id', '<', record.id), ('product_id', '=', record.product_id.id),
-                                    ('state', '=', 'done')], order='date asc').filtered(lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
+                    movs_origen = movs_ant.filtered(lambda y: (y.localidad_origen == record.localidad_origen and '/OUT/' in y.reference) #Salidas
                                     or (y.localidad_destino == record.localidad_origen and '/IN/' in y.reference) # entradas
                                     or ((y.localidad_origen == record.localidad_origen or y.localidad_destino == record.localidad_origen) and ('/INT/' in y.reference or 'INT' in y.reference)) # Internas
                                     or (y.localidad_destino == record.localidad_origen and y.location_id.id == 14) # Ajustes
@@ -115,7 +96,7 @@ class StockMoveLine(models.Model):
                     record.entradas_destino = record.qty_done
                     record.saldo_origen = saldo_movs_origen - record.salidas_origen
                     record.saldo_destino = saldo_movs_destino + record.entradas_destino
-
+                movs_ant|=record
 
 
 
