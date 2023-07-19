300cf7069bc6f4dd13d9d6a7da98e23b251835c8
jonathanalfaro <jonas4784@gmail.com>
2023-07-19 13:11:51 -0600

    update consolidacion_venta, Primera parte de la consolidación de las líneas de orden de venta

--- consolidacion_venta/wizard/consolidacion_wizard.py
+++ consolidacion_venta/wizard/consolidacion_wizard.py
@@ -10,21 +10,46 @@ class ConsolidacionWizard(models.Model):
     lines = fields.Many2many('wizard.consolidation.line')
     sale_orders = fields.Many2many('sale.order', string='Pedidos')
     total = fields.Float(string='Total')
+    subtotal = fields.Float(string='Subtotal')
 
     def _compute_lines(self):
         for record in self:
             order_lines = self.sale_orders.mapped('order_line')
             consolidacion_lines = []
-            for line in order_lines:
-                consolidacion_lines.append({
-                    'product_id': line.product_id.id
-                    , 'quantity': line.product_uom_qty
-                    , 'price_unit': line.price_unit
+            productos = order_lines.mapped('product_id')
+            lista_productos = []
+            for producto in productos:
+                tot = 0.0
+                cantidades_productos = order_lines.filtered(lambda x: x.product_id == producto).mapped('product_uom_qty')
+                precios_unitarios = order_lines.filtered(lambda x: x.product_id == producto).mapped('price_unit')
+                for i,c in enumerate(cantidades_productos):
+                    tot += cantidades_productos[i] *  precios_unitarios[i]
+                cantidad_productos = sum(cantidades_productos)
+                precio_unitario_prom = tot / cantidad_productos
+                sale_order_char = ', '.join(order_lines.filtered(lambda x: x.product_id == producto).mapped('order_id.name'))
+                orden_compra = ', '.join(order_lines.filtered(lambda x: x.product_id == producto and x.order_id.x_studio_n_orden_de_compra).mapped('order_id.x_studio_n_orden_de_compra'))
+                lista_productos.append({
+                    'product_id': producto.id
+                    , 'quantity': cantidad_productos
+                    , 'price_unit': precio_unitario_prom
+                    , 'orden_compra': orden_compra
+                    , 'sale_order_char': sale_order_char
                     , 'sequence': 10
                     , 'wizard_id': record.id
                 })
 
-            lines = self.env['wizard.consolidation.line'].create(consolidacion_lines)
+            # for line in order_lines:
+            #     consolidacion_lines.append({
+            #         'product_id': line.product_id.id
+            #         , 'quantity': line.product_uom_qty
+            #         , 'price_unit': line.price_unit
+            #         , 'sequence': 10
+            #         , 'wizard_id': record.id
+            #         , 'sale_order':line.order_id.id
+            #         , 'orden_compra': line.order_id.x_studio_n_orden_de_compra
+            #     })
+
+            lines = self.env['wizard.consolidation.line'].create(lista_productos)
             if lines:
                 record['lines'] = lines
             print()
@@ -47,4 +72,15 @@ class ConsolidacionWizardLine(models.Model):
     quantity = fields.Float(string='Cantidad')
     price_unit = fields.Float(string='Precio Unitario')
     wizard_id = fields.Many2one('consolidacion.wizard')
+    sale_order = fields.Many2one('sale.order')
+    sale_order_char = fields.Char(string='Órdenes de venta')
+    orden_compra = fields.Char(string='Orden de compra')
+    subtotal = fields.Float(string='Subtotal', compute='_compute_totals')
+    taxes = fields.Float(string='Impuestos', compute='_compute_totals')
+    total = fields.Float(string='Total', compute='_compute_totals')
 
+    def _compute_totals(self):
+        for record in self:
+            record.subtotal = record.price_unit * record.quantity
+            record.taxes = record.subtotal * 0.16
+            record.total = record.subtotal + record.taxes
