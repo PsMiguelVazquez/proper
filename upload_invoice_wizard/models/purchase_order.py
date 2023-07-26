1e6e0c60eed7012261d8a225584490401f35c411
jonathanalfaro <jonas4784@gmail.com>
2023-07-26 10:24:12 -0600

    UPDATE upload_invoice_wizard, Se agregó la opción de subir facturas de proveedor en las compras

--- upload_invoice_wizard/models/purchase_order.py
+++ upload_invoice_wizard/models/purchase_order.py
@@ -3,17 +3,17 @@
 from odoo import models, fields, _
 from odoo.exceptions import UserError
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-    adjuntos_so = fields.Many2many('ir.attachment')
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    adjuntos_po = fields.Many2many('ir.attachment')
 
 
     def upload_invoice(self):
-        if self.env['account.move'].search([('sale_id','in',self.ids)]).filtered(lambda x: x.state == 'posted'):
+        if self.env['account.move'].search([('invoice_origin','in',self.mapped('name')),('move_type','=','in_invoice')]).filtered(lambda x: x.state == 'posted'):
             raise UserError(_('No se puede subir una factura externa si la orden ya tiene una factura publicada'))
-        if self.filtered(lambda x: x.state != 'sale'):
-            raise UserError(_('No se puede subir una factura si el pedido no esta en el estado "Orden de venta"'))
-        w = self.env['upload.invoice.wizard'].create({'subtotal': 0.0, 'monto': 0.0, 'tipo':'sale_order'})
+        if self.filtered(lambda x: x.state != 'purchase'):
+            raise UserError(_('No se puede subir una factura si el pedido no esta en el estado "Orden de compra"'))
+        w = self.env['upload.invoice.wizard'].create({'subtotal': 0.0, 'monto': 0.0, 'tipo': 'purchase_order'})
         view = self.env.ref('upload_invoice_wizard.view_upload_invoice_sale_form')
         return {
             'name': _('Asignar Facturas'),
