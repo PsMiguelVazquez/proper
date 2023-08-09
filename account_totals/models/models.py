a92321c43483545451caf0e6c574b62bb392197b
clopez-ola <clopez@grupoola.net>
2023-08-08 23:15:28 -0600

    totals

---
+++ account_totals/models/models.py
@@ -0,0 +1,31 @@
+from odoo import fields, models, api, _
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def _get_tax_totals(self, partner, tax_lines_data, amount_total, amount_untaxed, currency):
+        r = super(AccountMove, self)._get_tax_totals(partner, tax_lines_data, amount_total, amount_untaxed, currency)
+        for k in list(r.keys()):
+            data = r.get(k)
+            if type(data) == float:
+                r[k] = round(data, 2)
+            if type(data) == str:
+                amount = r[k].split(self.currency_id.symbol)
+                monto = round(float(amount[1].replace(',', '')), 2)
+                r[k] = str(self.currency_id.symbol)+"\xa0"+f"{monto:,.2f}"
+            if type(data) == dict:
+                for tax in list(r[k].keys()):
+                    for li in r[k][tax]:
+                        for taxlis in list(li.keys()):
+                            data = li.get(taxlis)
+                            if type(data) == float:
+                                li[taxlis] = round(data, 2)
+                            if type(data) == str:
+                                if self.currency_id.symbol in data:
+                                    amount = li[taxlis].split(self.currency_id.symbol)
+                                    monto = round(float(amount[1].replace(',', '')), 2)
+                                    li[taxlis] = str(self.currency_id.symbol) + "\xa0" + f"{monto:,.2f}"
+        return r
+
