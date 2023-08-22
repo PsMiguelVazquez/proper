d5d008ea15192f763d04a9092e48a761df1c7b10
jonathanalfaro <jonas4784@gmail.com>
2023-08-22 14:17:06 -0600

    ADD factoraje_financiero, Nuevo módulo para la operación de factoraje financiero

---
+++ consolidacion_compras/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Consolidación de órdenes de compra",
+
+    'summary': """
+        Consolida las órdenes de compra seleccionadas, cambiando el estado de las ordenes
+        consolidadas a consolidado
+        """,
+
+    'description': """
+        Permite consolidar órdenes de compra
+    """,
+
+    'author': "Jonathan Alfaro",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'purchase'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'wizard/consolidacion_compras_view.xml',
+        'security/ir.model.access.csv'
+    ],
+}
---
+++ consolidacion_compras/models/purchase_order.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import datetime
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    state = fields.Selection([
+        ('draft', 'RFQ'),
+        ('sent', 'RFQ Sent'),
+        ('to approve', 'To Approve'),
+        ('purchase', 'Purchase Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+        ('consolidate', 'Consolidada')
+    ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
+
+
+    def view_consolidate_purchase_wizard(self):
+        purchase_orders = self.env['purchase.order'].browse(self.env.context.get('active_ids'))
+        if len(purchase_orders.mapped('partner_id')) > 1:
+            raise UserError('No se pueden consolidar órdenes de diferentes proveedores')
+        if len(purchase_orders) == 1:
+            raise UserError('Se deben consolidar 2 o más órdenes')
+        if len(list(set(purchase_orders.mapped('state')))) > 1 and purchase_orders.mapped('state')[0] != 'draft':
+            raise UserError('No se pueden consolidar órdenes que no están en estado borrador')
+        partner = purchase_orders.mapped('partner_id')
+        purchase_order_lines = purchase_orders.mapped('order_line')
+        w = self.env['consolidacion.compras.wizard'].sudo().create(
+            {'partner_id': partner.id, 'purchase_orders': purchase_orders, 'purchase_order_lines': purchase_order_lines})
+        view = self.env.ref('consolidacion_compras.view_consolidacion_compra_wizard_form')
+        context = dict(self.env.context)
+        context['form_view_initial_mode'] = 'edit'
+        context['view_no_maturity'] = False
+        return {
+            'name': _('Consolidar'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'consolidacion.compras.wizard',
+            'view_mode': 'tree, form',
+            'res_id': w.id,
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'context': context
+        }
