f3d26e5562a457c2eaaf1c3cc72da07e7287f0e9
jonathanalfaro <jonas4784@gmail.com>
2023-08-24 17:38:40 -0600

    UPDATE partner_account_creditor, Se agrega el campo de cuenta de acreedor en la vista de partners

---
+++ partner_account_creditor/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cuenta de acreedor para el partner",
+
+    'summary': """
+                Agrega la opción de configurar la cuenta de acreedor para los clientes/proveedores
+            """,
+
+    'description': """
+        Agrega la opción de configurar la cuenta de acreedor para los clientes/proveedores necesaria para el proceso de factoraje
+    """,
+
+    'author': "Jonathan Alfaro",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'views/res_partner.xml',
+    ],
+
+}
