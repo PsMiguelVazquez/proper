2afd23ab915704e286235606675d49158f6623ac
jonathanalfaro <jonas4784@gmail.com>
2023-08-24 11:33:47 -0600

    UPDATE block_edit_sale_order, Se bloquea la órden para edición si no está en estado borrador

--- block_edit_sale_order/models/sale_order.py
+++ block_edit_sale_order/models/sale_order.py
@@ -8,9 +8,9 @@ from datetime import datetime
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    edit_blocked = fields.Boolean('Bloqueado', default=False, compute='_compute_edit_blocked', store=True)
+    edit_blocked = fields.Boolean('Bloqueado', default=False, compute='_compute_edit_blocked')
 
     @api.depends('state')
     def _compute_edit_blocked(self):
         for record in self:
-            record.edit_blocked = record.state in ['sale', 'cancel']
+            record.edit_blocked = record.state not in ['draft']
