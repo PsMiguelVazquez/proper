eefe6559cde92e606344f0ee489419f548d24c29
rodrigo <rodrigo.vargas@novucentral.com>
2024-07-03 17:16:33 -0600

    Se actualiza proyecto upload_invoice_wizard a petición de proper

--- upload_invoice_wizard/models/sale_order.py
+++ upload_invoice_wizard/models/sale_order.py
@@ -6,11 +6,12 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     adjuntos_so = fields.Many2many('ir.attachment')
+    credit_notes = fields.Many2many('account.move', string='Notas de crédito relacionadas', compute='get_credit_notes')
 
 
     def upload_invoice(self):
-        if self.env['account.move'].search([('sale_id','in',self.ids)]).filtered(lambda x: x.state == 'posted'):
-            raise UserError(_('No se puede subir una factura externa si la orden ya tiene una factura publicada'))
+        if self.env['account.move'].search([('sale_id','in',self.ids)]).filtered(lambda x: x.state in ['posted', 'draft']):
+            raise UserError(_('No se puede subir una factura externa si la orden ya tiene una factura o borrador de factura'))
         if self.filtered(lambda x: x.state != 'sale'):
             raise UserError(_('No se puede subir una factura si el pedido no esta en el estado "Orden de venta"'))
         w = self.env['upload.invoice.wizard'].create({'subtotal': 0.0, 'monto': 0.0, 'tipo':'sale_order', 'margen': 1.0})
