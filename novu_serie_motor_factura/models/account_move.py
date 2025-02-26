8a3e8f0d4b26cb6a00e02b6c51a998be37954e01
rvargasNovu <rodrigo.vargas@novucentral.com>
2025-02-26 02:26:19 +0000

    Se actualiza modulo novu_serie_motor_factura para completar el proceso correcto de setear numero de serie y motor, se actualiza reporte moto 2 para mostrar esos numeros.

--- novu_serie_motor_factura/models/account_move.py
+++ novu_serie_motor_factura/models/account_move.py
@@ -28,3 +28,73 @@ class AccountMove(models.Model):
                 "No se puede confirmar la factura. El número de serie y número de motor son obligatorios para los productos {}, verifique la pestaña Número de Serie y Motor, y proporcione los datos faltantes.".format(faltantes_str)
             )
         return super(AccountMove, self).action_post()
+
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        moves = super().create(vals_list)
+        for move in moves:
+            if move.move_type == 'out_invoice' and move.state == 'draft':  # Solo en facturas de cliente en borrador
+                move._create_series_motor()
+        return moves
+
+        
+
+    def write(self, vals):
+        result = super().write(vals)
+        for move in self:
+            if move.move_type == 'out_invoice' and move.state == 'draft':  # Solo si sigue en borrador
+                move._create_series_motor()
+        return result
+
+        return res
+
+
+    def _create_series_motor(self):
+        """ Crea las líneas en product.serie.motor si no existen """
+        self.ensure_one()
+        series_motor = []
+        for line in self.invoice_line_ids:
+            if line.product_id.categ_id.name in ['MOTOCICLETAS', 'TRANSPORTE']:
+                existing_series = self.env['product.serie.motor'].search([
+                    ('move_id', '=', self.id),
+                    ('invoice_line_id', '=', line.id),
+                    ('product_id', '=', line.product_id.id)
+                ])
+                if not existing_series:
+                    for _ in range(int(line.quantity)):
+                        series_motor.append({
+                            'move_id': self.id,
+                            'invoice_line_id': line.id,
+                            'product_id': line.product_id.id,
+                            'serial_number': '',
+                            'engine_number': '',
+                        })
+
+                else:
+                    current_count = len(existing_series)
+                    new_count = int(line.quantity)
+                    _logger.error(f"current_count {current_count}, new_count {new_count}")
+                    
+                    if new_count > current_count:
+                        # Se agregan los números de serie faltantes
+                        series_motor = [{
+                            'move_id': line.move_id.id,
+                            'invoice_line_id': line.id,
+                            'product_id': line.product_id.id,
+                            'serial_number': '',
+                            'engine_number': '',
+                            
+                        } for _ in range(new_count - current_count)]
+
+                        #self.env['product.serie.motor'].sudo().create(series_motor)
+                        _logger.error(f"series_motor {series_motor}")
+                        
+                    elif new_count < current_count:
+                        # Se eliminan los números de serie sobrantes
+                        series_to_remove = existing_series[:current_count - new_count]
+                        series_to_remove.unlink()
+                        
+        if series_motor:
+            self.env['product.serie.motor'].sudo().create(series_motor)
--- novu_serie_motor_factura/models/account_move_line.py
+++ novu_serie_motor_factura/models/account_move_line.py
@@ -5,82 +5,21 @@ _logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
-
-   
-    @api.model_create_multi
-    def create(self, vals_list):
-        _logger.error(f"Entre Create {vals_list}")
-        lines = super(AccountMoveLine, self).create(vals_list)
-
-        series_motor = []
-        for line in lines:
-            _logger.error(f"Create for line in lines {line.product_id.categ_id.name}")
-            # Verificar si la línea tiene un producto y si es de la categoría 'MOTOCICLETAS'
-            if line.product_id and line.product_id.categ_id.name == 'MOTOCICLETAS' or line.product_id.categ_id.name == 'TRANSPORTE':
-                existing_series = self.env['product.serie.motor'].search([('invoice_line_id', '=', line.id), ('product_id', '=', line.product_id.id)])
-                _logger.error(f"Create existing_series {existing_series}")
-                if not existing_series:
-                    for _ in range(int(line.quantity)):  # Crear tantos registros como cantidad en la línea
-                        series_motor.append({
-                            'move_id': line.move_id.id,
-                            'invoice_line_id': line.id,
-                            'product_id': line.product_id.id,
-                            'serial_number': '',  # Se puede llenar luego
-                            'engine_number': '',  # Se puede llenar luego
-                        })
-
-        # Si hay registros de serie por crear, se insertan en `product.serie.motor`
-        if series_motor:
-            self.env['product.serie.motor'].sudo().create(series_motor)
-
-        return lines
-        
-
-    def write(self, vals):
-        _logger.error(f"Entre Write {vals}")
-        res = super(AccountMoveLine,self).write(vals)
-        if 'quantity' in vals:
-            _logger.error(f"Entre if quantity")
-            for line in self:
-                if line.product_id and line.product_id.categ_id.name == 'MOTOCICLETAS' or line.product_id.categ_id.name == 'TRANSPORTE':
-                    _logger.error(f"Entre if line.product_id and line.product_id.categ_id.name == 'MOTOCICLETAS'")
-                    existing_series = self.env['product.serie.motor'].search([('invoice_line_id', '=', line.id), ('product_id', '=', line.product_id.id)])
-                    _logger.error(f"existing_series {existing_series}")
-                    current_count = len(existing_series)
-                    new_count = int(line.quantity)
-
-                    _logger.error(f"current_count {current_count}, new_count {new_count}")
-                    
-                    if new_count > current_count:
-                        # Se agregan los números de serie faltantes
-                        series_motor = [{
-                            'move_id': line.move_id.id,
-                            'invoice_line_id': line.id,
-                            'product_id': line.product_id.id,
-                            'serial_number': '',
-                            'engine_number': '',
-                            
-                        } for _ in range(new_count - current_count)]
-
-                        self.env['product.serie.motor'].sudo().create(series_motor)
-                        _logger.error(f"series_motor {series_motor}")
-                        
-                    elif new_count < current_count:
-                        # Se eliminan los números de serie sobrantes
-                        series_to_remove = existing_series[:current_count - new_count]
-                        series_to_remove.unlink()
-
-        return res
-
-
+    
+    serie_motor_ids = fields.One2many(
+        'product.serie.motor', 
+        'invoice_line_id', 
+        string="Series de Motor"
+    )
+    
     @api.model
     def unlink(self):
-        # Detectar si alguna de las líneas está relacionada con tu modelo personalizado
+        deleted_lines = []
         for line in self:
-            # Aquí verificamos si la línea tiene alguna relación con 'product.serie.motor'
             related_motor = self.env['product.serie.motor'].search([('invoice_line_id', '=', line.id),('product_id', '=', line.product_id.id)])
             if related_motor:
-                related_motor.unlink()  # Eliminar los registros relacionados de 'product.serie.motor'
+                deleted_lines.append(line.id)
+                related_motor.unlink() 
+
         
-        # Llamar al método unlink original para eliminar la línea de la factura
         return super(AccountMoveLine, self).unlink()
