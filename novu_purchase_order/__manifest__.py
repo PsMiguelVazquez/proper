357d80c6362a59e8430ad541d86b5881a0c80023
rvargasNovu <rodrigo.vargas@novucentral.com>
2025-06-06 20:54:22 +0000

    Se actualiza modulo novu_sale_order, se agrega modificacion a la vista para que posiciones los campos donde deben ir, se agrega modulo novu_purchase_order, para agregar los campos necesarios para estabilizacion del modulo compras V18

---
+++ novu_purchase_order/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Novu Purchase Order',
+    'version': '18.0.0.1',
+    'category': '',
+    'license': 'OPL-1',
+    'summary': 'Agrega campos y cambios necesarios al modulo de compras',
+
+    'author': 'Mayra Carrillo',
+    'maintainer': '',
+    'website': '',
+
+    'depends': ['purchase', 'web_studio', 'sale_purchase_confirm'],
+    'data': [
+            'views/purchase_order.xml',
+    ],
+    'assets': {},
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'active': False,
+
+}
