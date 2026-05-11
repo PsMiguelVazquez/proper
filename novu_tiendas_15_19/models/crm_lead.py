2d10f43559d20b0c466059a6ce61013d3e5edc92
rvargasNovu <rodrigo.vargas@novucentral.com>
2026-05-11 17:37:16 +0000

    EASM cambios estables para el crm

--- novu_tiendas_15_19/models/crm_lead.py
+++ novu_tiendas_15_19/models/crm_lead.py
@@ -1,10 +1,24 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     captado_en = fields.Char(string="Captado en")
     mensaje = fields.Char(string="Mensaje de interes")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('captado_en') == 'PROPER V19':
+            vendedor = self.env['res.users'].search([
+                ('login', '=', 'soporte@a-pro.com.mx')
+            ], limit=1)
+
+            if vendedor:
+                vals['user_id'] = vendedor.id
+
+        return super(CrmLead, self).create(vals)
