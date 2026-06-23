4db9081673663c338cebdbea3055eafb26a25e1a
rvargasNovu <rodrigo.vargas@novucentral.com>
2026-06-23 00:57:00 +0000

    MBCT: Se actualiza modulo novu_tiendas_15_19, se agregan campos para sincronización desde formulario de registrarse de la V19

--- novu_tiendas_15_19/models/crm_lead.py
+++ novu_tiendas_15_19/models/crm_lead.py
@@ -12,12 +12,37 @@ class CrmLead(models.Model):
     mensaje = fields.Char(string="Mensaje de interes")
     tamanio_empresa = fields.Selection(
         string="Tamaño de la empresa",
+        readonly=True,
         selection=[('1_50', '1 - 50 Empleados'),
                     ('51_200', '51 - 200 Empleados'),
                     ('201_1000', '201 - 1000 Empleados'),
                     ('1000_plus', '+ 1000 Empleados')]
                    )
 
+    rfc_empresa = fields.Char(
+        string='RFC', 
+        help="RFC")
+    
+    industria = fields.Char(
+        string="Industria",
+        readonly=True
+                   )
+
+    tipo_proyecto = fields.Char(
+        string="Tipo de proyecto",
+        readonly=True,
+        )
+
+    volumen_compra_estimado = fields.Char(
+        string="Volumen de compra estimado",
+        readonly=True,
+            )
+    
+    fecuencia_compra_estimado = fields.Char(
+        string="Frecuencia de compra estimada",
+        readonly=True,
+        )
+    
     @api.model
     def create(self, vals):
         if vals.get('captado_en') == 'PROPER V19':
