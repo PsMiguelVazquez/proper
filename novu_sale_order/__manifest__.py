fa75cf6d5a4bde9e5b8427404d3beb2fb87540f0
rvargasNovu <rodrigo.vargas@novucentral.com>
2026-07-16 19:31:05 +0000

    MBCT: Se agrega modulo novu_sale_order para solventar temas de mkp

---
+++ novu_sale_order/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Novu sale_order',
+    'version': '19.0.0.1',
+    'category': '',
+    'license': 'OPL-1',
+    'summary': 'Agrega campos necesarios en la venta',
+
+    'author': 'Mayra Carrillo',
+    'maintainer': '',
+    'website': '',
+
+    'depends': ['sale', 'vehiculos', 'web_studio', 'sale_purchase_confirm','sale_stock','sale_line_date_planned','sale_management','mail', 'account','base'],
+    'data': [
+            'views/sale_order_view.xml',
+            'views/sale_order_mkp.xml',
+            'views/sale_order_action_mkp.xml',
+            'security/sale_order_groups.xml',
+    ],
+    'assets': {},
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'active': False,
+
+}
