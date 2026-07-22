9a1e2a40b005f1877cfe8bca6a01dbcf04540c81
rvargasNovu <rodrigo.vargas@novucentral.com>
2026-07-22 20:20:55 +0000

    MBCT: Se actualizan modulos novu_sale_order, sale_purchase_confirm y studio_fields_v19, para activar automatizaciones de ventas que se desactivan en cada build, corregir error de wizard de rechazo de orden.

--- novu_sale_order/__manifest__.py
+++ novu_sale_order/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Novu sale_order',
-    'version': '19.0.0.5',
+    'version': '19.0.0.6',
     'category': '',
     'license': 'OPL-1',
     'summary': 'Agrega campos necesarios en la venta',
