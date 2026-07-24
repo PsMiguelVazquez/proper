7c9ae35a6770a40eb651d3a02a88fae2e2a0b97d
CarrilloMayra <mayra.carrillo@novucentral.com>
2026-07-24 20:00:39 +0000

    MBCT: se actualiza modulo studio_fields_v19, se ajustan mas campos para que sirvan los reportes.

--- studio_fields_v19/migrations/0.0.0/pre-fix_broken_stock_picking_reports.py
+++ studio_fields_v19/migrations/0.0.0/pre-fix_broken_stock_picking_reports.py
@@ -14,19 +14,25 @@ _STOCK_PICKING_QTY_DONE_RE = re.compile(r'\b(?:qty_done|quantity_done)\b')
 _STOCK_PICKING_MOVE_LINE_WITHOUT_PACKAGE_RE = re.compile(r'\bmove_line_ids_without_package\b')
 _STOCK_PICKING_L10N_MX_EDI_STATUS_RE = re.compile(r'\bl10n_mx_edi_status\b')
 _STOCK_PICKING_TRANSPORT_PERM_SCT_RE = re.compile(r'\btransport_perm_sct\b')
+_STOCK_PICKING_TRANSPORT_INSURER_RE = re.compile(r'\btransport_insurer\b')
+_STOCK_PICKING_PACKAGE_LEVEL_IDS_RE = re.compile(r'[a-zA-Z_][a-zA-Z0-9_.()]*\.package_level_ids')
+_STOCK_PICKING_DESCRIPTION_BOM_LINE_RE = re.compile(r'\bdescription_bom_line\b')
 
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     views = env['ir.ui.view'].search([
         ('type', '=', 'qweb'),
-        '|', '|', '|', '|', '|',
+        '|', '|', '|', '|', '|', '|', '|', '|',
         ('arch_db', 'like', 'move_lines'),
         ('arch_db', 'like', 'move_ids_without_package'),
         ('arch_db', 'like', 'qty_done'),
         ('arch_db', 'like', 'quantity_done'),
         ('arch_db', 'like', 'l10n_mx_edi_status'),
         ('arch_db', 'like', 'transport_perm_sct'),
+        ('arch_db', 'like', 'transport_insurer'),
+        ('arch_db', 'like', 'package_level_ids'),
+        ('arch_db', 'like', 'description_bom_line'),
     ])
     for view in views:
         arch = view.arch_db
@@ -38,5 +44,8 @@ def migrate(cr, version):
         new_arch = _STOCK_PICKING_QTY_DONE_RE.sub('quantity', new_arch)
         new_arch = _STOCK_PICKING_L10N_MX_EDI_STATUS_RE.sub('l10n_mx_edi_cfdi_state', new_arch)
         new_arch = _STOCK_PICKING_TRANSPORT_PERM_SCT_RE.sub('l10n_mx_transport_perm_sct', new_arch)
+        new_arch = _STOCK_PICKING_TRANSPORT_INSURER_RE.sub('l10n_mx_transport_insurer', new_arch)
+        new_arch = _STOCK_PICKING_PACKAGE_LEVEL_IDS_RE.sub('False', new_arch)
+        new_arch = _STOCK_PICKING_DESCRIPTION_BOM_LINE_RE.sub('description_picking', new_arch)
         if new_arch != arch:
             view.write({'arch_db': new_arch})
